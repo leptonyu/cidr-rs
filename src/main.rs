bf98c1234112c90e0fc374faf538c82d89edd268
Daniel YU <i@icymint.me>
2024-01-19 18:10:07 +0800

    allow reduce

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,101 @@
 use std::{
     collections::{btree_set::Iter, BTreeSet},
-    io::BufRead,
+    fs::File,
+    io::{BufRead, Read},
     net::{Ipv4Addr, Ipv6Addr},
 };
 
 use cfg_rs::*;
 
+#[derive(FromConfig, Debug)]
+pub struct Config {
+    #[config(default = false)]
+    reverse: bool,
+    exclude: Option<String>,
+}
+
 fn main() -> Result<(), ConfigError> {
+    let config = init_args(Configuration::with_predefined_builder()).init()?;
+    let conf: Config = config.get("")?;
     let mut list = SubnetList::default();
-    list.read_stdin()?;
-    list.shrink();
+    // println!("{:?}", conf);
+    list.read_stdin(conf.reverse, conf.exclude)?;
     for subnet in list.iter() {
         println!("{}", subnet.to_string());
     }
     Ok(())
 }
 
+fn init_args(mut builder: PredefinedConfigurationBuilder) -> PredefinedConfigurationBuilder {
+    for arg in std::env::args() {
+        if arg.find("--") == Some(0) {
+            builder = add_arg(&arg["--".len()..], builder);
+        }
+    }
+    builder
+}
+
+fn add_arg(
+    arg: &str,
+    mut builder: PredefinedConfigurationBuilder,
+) -> PredefinedConfigurationBuilder {
+    if let Some(r) = arg.find('=') {
+        let key = &arg[0..r];
+        let val = &arg[r + 1..];
+        builder = builder.set(
+            key.replace('-', "_"),
+            if val.is_empty() {
+                "true".to_string()
+            } else {
+                val.to_owned()
+            },
+        );
+    }
+    builder
+}
+
 #[derive(PartialEq, Eq, PartialOrd, Ord, Clone, Copy)]
 struct Subnet {
     net: Result<u32, u128>,
     mask: u8,
+    tag: u8,
 }
 
 macro_rules! common_parse_fn {
     ($tp:ident, $tpa:ident , $mp:literal, $name: ident, $new: ident, $try_ip: ident) => {
-        fn $name(v: &mut SubnetList, mut from: $tp, mut to: $tp) -> Result<(), ConfigError> {
+        fn $name(
+            v: &mut SubnetList,
+            mut from: $tp,
+            mut to: $tp,
+            tag: Option<u8>,
+        ) -> Result<(), ConfigError> {
             let mut mask = $mp;
             while from <= to {
                 if from == to {
-                    v.insert(Subnet::$new(from << ($mp - mask), mask));
+                    if from != 0 || mask > 0 {
+                        from <<= ($mp - mask);
+                    }
+                    let mut item = Subnet::$new(from, mask);
+                    if let Some(t) = tag {
+                        item.tag = t;
+                    }
+                    v.insert(item);
                     break;
                 }
                 if from & 1 == 1 {
-                    v.insert(Subnet::$new(from << ($mp - mask), mask));
+                    let mut item = Subnet::$new(from << ($mp - mask), mask);
+                    if let Some(t) = tag {
+                        item.tag = t;
+                    }
+                    v.insert(item);
                     from += 1;
                 }
                 if to & 1 == 0 {
-                    v.insert(Subnet::$new(to << ($mp - mask), mask));
+                    let mut item = Subnet::$new(to << ($mp - mask), mask);
+                    if let Some(t) = tag {
+                        item.tag = t;
+                    }
+                    v.insert(item);
                     to -= 1;
                 }
                 while from & 1 == 0 && to & 1 == 1 {
@@ -59,12 +118,17 @@ macro_rules! common_parse_fn {
 }
 impl Subnet {
     fn new_v4(net: u32, mask: u8) -> Self {
-        Self { net: Ok(net), mask }
+        Self {
+            net: Ok(net),
+            mask,
+            tag: 0,
+        }
     }
     fn new_v6(net: u128, mask: u8) -> Self {
         Self {
             net: Err(net),
             mask,
+            tag: 0,
         }
     }
 
@@ -84,7 +148,7 @@ impl Subnet {
     common_parse_fn!(u32, Ipv4Addr, 32, parse_ipv4_range, new_v4, try_ipv4);
     common_parse_fn!(u128, Ipv6Addr, 128, parse_ipv6_range, new_v6, try_ipv6);
 
-    fn parse_range(s: &str, v: &mut SubnetList) -> Result<(), ConfigError> {
+    fn parse_range(s: &str, v: &mut SubnetList, tag: Option<u8>) -> Result<(), ConfigError> {
         let split: Vec<&str> = Self::prepare_str(s).split(',').take(2).collect();
         let from = Subnet::parse_subnet(split[0])?
             .ok_or(ConfigError::RefValueRecursiveError)?
@@ -93,8 +157,8 @@ impl Subnet {
             .ok_or(ConfigError::RefValueRecursiveError)?
             .net;
         match (from, to) {
-            (Ok(from), Ok(to)) => Self::parse_ipv4_range(v, from, to),
-            (Err(from), Err(to)) => Self::parse_ipv6_range(v, from, to),
+            (Ok(from), Ok(to)) => Self::parse_ipv4_range(v, from, to, tag),
+            (Err(from), Err(to)) => Self::parse_ipv6_range(v, from, to, tag),
             _ => panic!("Error"),
         }
     }
@@ -107,11 +171,14 @@ impl Subnet {
         s
     }
 
-    fn parse(s: &str, vec: &mut SubnetList) -> Result<(), ConfigError> {
+    fn parse(s: &str, vec: &mut SubnetList, tag: Option<u8>) -> Result<(), ConfigError> {
         if s.contains(',') {
-            return Self::parse_range(s, vec);
+            return Self::parse_range(s, vec, tag);
         }
-        if let Some(x) = Self::parse_subnet(s)? {
+        if let Some(mut x) = Self::parse_subnet(s)? {
+            if let Some(t) = tag {
+                x.tag = t;
+            }
             vec.insert(x);
         }
         Ok(())
@@ -120,6 +187,9 @@ impl Subnet {
     pub fn contains(&self, target: &Self) -> bool {
         if self.mask > target.mask {
             return false;
+        }
+        if self.mask == 0 {
+            return true;
         }
         match (self.net, target.net) {
             (Ok(a), Ok(b)) => {
@@ -171,13 +241,41 @@ impl ToString for Subnet {
 struct SubnetList(BTreeSet<Subnet>);
 
 impl SubnetList {
-    pub fn read_stdin(&mut self) -> Result<(), ConfigError> {
+    pub fn read_stdin(
+        &mut self,
+        reverse: bool,
+        exclude: Option<String>,
+    ) -> Result<(), ConfigError> {
         let stdin = std::io::stdin();
         let lines = stdin.lock().lines();
         for line in lines {
-            Subnet::parse(&line?, self).ok();
+            Subnet::parse(&line?, self, None).ok();
         }
+        self.shrink();
+        if let Some(file) = exclude {
+            let mut file = File::open(file)?;
+            let mut buf = String::new();
+            file.read_to_string(&mut buf)?;
+            let mut new = SubnetList::default();
+            for line in buf.lines() {
+                Subnet::parse(line, &mut new, None).ok();
+            }
+            new.shrink();
+            self.merge(new);
+        } else if reverse {
+            let ret = self.gap();
+            let _ = std::mem::replace(self, ret);
+        }
+
         Ok(())
+    }
+
+    fn merge(&mut self, new: SubnetList) {
+        for mut item in new.gap().0.into_iter() {
+            item.tag = 1;
+            self.insert(item);
+        }
+        self.shrink();
     }
 
     pub fn insert(&mut self, subnet: Subnet) -> bool {
@@ -185,11 +283,13 @@ impl SubnetList {
     }
 
     pub fn shrink(&mut self) {
-        let mut vec = vec![];
+        let mut vec: Vec<Subnet> = vec![];
         let mut last: Option<Subnet> = None;
         for i in self.0.iter() {
-            if let Some(l) = &last {
+            if let Some(l) = &mut last {
                 if l.contains(i) {
+                    let len = vec.len();
+                    vec[len - 1].tag = 0;
                     continue;
                 }
             }
@@ -197,17 +297,56 @@ impl SubnetList {
             merge_vec(&mut vec, *i);
         }
         self.0.clear();
-        self.0.extend(vec);
+        for i in vec {
+            if i.tag == 0 {
+                self.insert(i);
+            }
+        }
     }
 
     pub fn iter(&self) -> Iter<Subnet> {
         self.0.iter()
     }
+
+    pub fn gap(&self) -> Self {
+        let mut last_v4 = (0, u32::MAX);
+        let mut last_v6 = (0, u128::MAX);
+        let mut list = SubnetList::default();
+        for item in self.iter() {
+            match item.net {
+                Ok(s) => {
+                    if s > last_v4.0 {
+                        let _ = Subnet::parse_ipv4_range(&mut list, last_v4.0, s - 1, None);
+                    }
+                    let x = s + (1 << (32 - item.mask));
+                    if x > last_v4.0 {
+                        last_v4.0 = x;
+                    }
+                }
+                Err(s) => {
+                    if s > last_v6.0 {
+                        let _ = Subnet::parse_ipv6_range(&mut list, last_v6.0, s - 1, None);
+                    }
+                    let x = s + (1 << (128 - item.mask));
+                    if x > last_v6.0 {
+                        last_v6.0 = x;
+                    }
+                }
+            }
+        }
+        if last_v4.0 < last_v4.1 {
+            let _ = Subnet::parse_ipv4_range(&mut list, last_v4.0, last_v4.1, None);
+        }
+        if last_v6.0 < last_v6.1 {
+            let _ = Subnet::parse_ipv6_range(&mut list, last_v6.0, last_v6.1, None);
+        }
+        list
+    }
 }
 
 fn merge_vec(vec: &mut Vec<Subnet>, mut i: Subnet) {
     while let Some(mut l) = vec.pop() {
-        if l.is_next(&i) {
+        if l.tag == i.tag && l.is_next(&i) {
             l.mask -= 1;
             i = l;
         } else {
@@ -285,14 +424,51 @@ mod tests {
     #[test]
     fn range_test() -> Result<(), ConfigError> {
         let mut list = SubnetList::default();
-        Subnet::parse("223.255.229.0,223.255.230.255,", &mut list)?;
+        Subnet::parse("223.255.229.0,223.255.230.255,", &mut list, None)?;
         Subnet::parse(
             "2c0f:fc00:b011::,2c0f:fc00:b011:ffff:ffff:ffff:ffff:ffff,",
             &mut list,
+            None,
         )?;
         for x in list.iter() {
             println!("{}", x.to_string());
         }
+        Ok(())
+    }
+
+    #[test]
+    fn test_gap() -> Result<(), ConfigError> {
+        let mut list = SubnetList::default();
+        insert!(list. "0.0.0.0/8");
+        insert!(list. "1.0.0.0/24");
+        print_list(&list.gap());
+        Ok(())
+    }
+
+    fn print_list(list: &SubnetList) {
+        for x in list.iter() {
+            println!("{}", x.to_string());
+        }
+    }
+
+    #[test]
+    fn test_merge() -> Result<(), ConfigError> {
+        let mut list = SubnetList::default();
+        insert!(list. "1.1.1.0/24");
+        insert!(list. "1.1.3.0/24");
+        insert!(list. "2.2.2.0/24");
+        insert!(list. "2.3.2.0/24");
+        list.shrink();
+        print_list(&list);
+        let mut excl = SubnetList::default();
+        insert!(excl. "0.0.0.0/8");
+        insert!(excl. "1.1.2.0/24");
+        excl.shrink();
+        println!("------ Gap List");
+        print_list(&excl.gap());
+        list.merge(excl);
+        println!("------ Mergr List");
+        print_list(&list);
         Ok(())
     }
 }
